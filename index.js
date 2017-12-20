8d1518cafe07ed9064f21d2fd83739f1887be480
David Espinoza <davidespinoza@att.net>
2017-12-20 03:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your code in this file!
 const currentUser = " ";
+const name = "david";
+document.write(name);
 const welcomeMessage = "Welcome to Flatbook, " + "!";
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "!";
 const shortGreeting = "Welcome, " + "!";
