9edc40bc076b520555df7d897a44a8184398edb5
David Espinoza <davidespinoza@att.net>
2017-12-20 03:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,3 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = "Welcome to Flatbook, " + "!";
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "!";
-const shortGreeting = "Welcome, " + currentUser.slice(1) + "!";
